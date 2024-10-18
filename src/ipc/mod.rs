6537752731535e2e49c1cd06d99f3deff1bdbf03
lbl <1791778603@qq.com>
2024-10-18 09:32:35 +0800

    feature = "web"

--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -3,6 +3,7 @@ use rustp2p::pipe::PipeWriter;
 
 pub mod common;
 
+#[cfg(feature = "web")]
 pub mod http;
 pub mod service;
 pub mod udp;
@@ -10,5 +11,7 @@ pub mod udp;
 pub async fn server_start(port: u16, pipe_writer: PipeWriter) -> anyhow::Result<()> {
     let api_service = ApiService::new(pipe_writer);
     udp::server::start(port, api_service.clone()).await?;
-    http::server::start(port, api_service).await
+    #[cfg(feature = "web")]
+    http::server::start(port, api_service).await?;
+    Ok(())
 }
