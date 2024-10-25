fb15b16cfe7b72441b049152036c3b0d3c0dda3d
lbl <1791778603@qq.com>
2024-10-25 17:11:02 +0800

    remove udp cmd server

--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -5,10 +5,8 @@ pub mod common;
 #[cfg(feature = "web")]
 pub mod http;
 pub mod service;
-pub mod udp;
 
 pub async fn server_start(addr: SocketAddr, api_service: ApiService) -> anyhow::Result<()> {
-    udp::server::start(addr, api_service.clone()).await?;
     #[cfg(feature = "web")]
     http::server::start(addr, api_service).await?;
     Ok(())
